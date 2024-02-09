7eba9351e721d85ecda7c3c28238d8ad7cf1a8bf
rtrelease24 <70213482+rtrelease24@users.noreply.github.com>
2024-02-09 14:31:26 -0500

    Update index.py

--- index.py
+++ index.py
@@ -16,20 +16,37 @@ with tab1:
 with tab2:
     st.title("Skills")
     st.write("## Programming Languages")
-    st.write("- Python\n- JavaScript\n- PHP\n- Web Development (HTML, CSS, Bootstrap)\n- SQL")
-    st.write("## Frameworks and Technologies")
-    st.write("- Streamlit\n- Bootstrap")
+    programming_languages = {
+        "Python": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+        "JavaScript": "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+        "PHP": "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+        "Web Development (HTML, CSS, Bootstrap)": "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+        "SQL": "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg"
+    }
+    for language, image_url in programming_languages.items():
+        col1, col2, col3 = st.columns([0.02, 0.1, 0.7])
+        with col1:
+            st.markdown("<style>img {pointer-events: none;} </style>", unsafe_allow_html=True)
+            st.write("•")
+        with col2:
+            st.markdown(f'<img src="{image_url}" style="width: 70px;">', unsafe_allow_html=True)
+        with col3:
+            st.write(language)
+        
 
 
 
 with tab3:
-    st.title("Major Projects")
+    st.title("Highschool Projects")
     st.write("## [Store POS](http://areawebsites.org/rtrelease/Portfolio/PHP/Store_POS/index.php)")
     st.write("A point-of-sale system for the school store.")
     st.write ("## [Pampered Pups](http://areawebsites.org/rTrelease/Portfolio/PHP/Pampered%20Pups/index.php)")
     st.write("A website for a dog grooming business.")
     st.write("## [Real Estate](http://areawebsites.org/rTrelease/Portfolio/PHP/Real%20Estate/index.php)")
     st.write("A website for a real estate business.")
+    st.write("## [Career Research](http://areawebsites.org/rTrelease/Portfolio/Powerpoint/Career%20Research.pdf)")
+    st.write("A presentation on potential careers.")
+    
     
         
 
